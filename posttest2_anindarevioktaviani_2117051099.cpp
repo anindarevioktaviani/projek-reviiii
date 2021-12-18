732d8823db2bb66c09fcdb490907e01b07defd98
anindarevioktaviani <92665686+anindarevioktaviani@users.noreply.github.com>
2021-12-19 06:26:34 +0700

    Add files via upload

---
+++ posttest2_anindarevioktaviani_2117051099.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+	
+	int a;
+	cout<<" masukin nilai : " ;
+	cin>>a;
+	
+	switch(a) {
+		case 1 :
+			cout <<"satu" <<endl ;
+		break ;
+		case 2 :
+			cout << "dua" <<endl ;
+		break ;
+		case 3 :
+			cout << "tiga"<<endl  ;
+		break ;
+		case 4 :
+			cout << "empat"<<endl ;
+		break ;
+		case 5 :
+			cout << " lima "<<endl ;
+		break ;
+		case 6 :
+			cout << " enam "<<endl ;
+		break ;
+		case 7 :
+			cout << " tujuh " <<endl ;
+		break  ;
+		case 8 :
+			cout << " delapan "<<endl ;
+		break  ;
+		case 9 :
+			cout << " sembilan "<<endl ;
+		break ;
+		default :
+			cout<<"hasil ; "<<a<<endl; 
+			
+	     
+			}
+			
+	
+
+	}
+	
